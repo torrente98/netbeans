2c1ebe45e4f76ee5ccbf078201b2deb88e69c2ec
torrente98 <torrenteyp@gmail.com>
2023-10-29 19:48:38 +0100

    Fichero subido

--- src/main/java/com/mycompany/prueba/Prueba_GITHUB.java
+++ src/main/java/com/mycompany/prueba/Prueba_GITHUB.java
@@ -8,7 +8,7 @@ package com.mycompany.prueba;
  *
  * @author Federico
  */
-public class Prueba {
+public class Prueba_GITHUB {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
